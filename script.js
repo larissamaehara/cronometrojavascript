f8fac9aa1d73a2f2dfb08d38260fa788886d0951
Larissa Maehara <larissalali.m@gmail.com>
2023-06-03 16:18:09 -0300

    cronometro

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const hoursEl = document.querySelector('#hours');
 const minutesEl = document.querySelector("#minutes");
 const secondsEl = document.querySelector("#seconds");
 const millisecondsEl = document.querySelector("#milliseconds");
@@ -7,6 +8,7 @@ const resumeBtn = document.querySelector("#resumeBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
 let interval;
+let hours = 0;
 let minutes = 0;
 let seconds = 0;
 let milliseconds = 0;
@@ -29,6 +31,11 @@ function startTimer() {
         minutes++;
         seconds = 0;
       }
+      if (minutes === 60) {
+        hours++;
+        minutes = 0;
+      }
+      hoursEl.innerHTML = formatMilliseconds(hours);
       minutesEl.innerHTML = formatTime(minutes);
       secondsEl.innerHTML = formatTime(seconds);
       millisecondsEl.innerHTML = formatMilliseconds(milliseconds);
@@ -52,13 +59,15 @@ function resumeTimer() {
 
 function resetTimer() {
   clearInterval(interval);
+  hours = 0;
   minutes = 0;
   seconds = 0;
   milliseconds = 0;
   isPaused = false;
+  hoursEl.innerHTML = "00";
   minutesEl.innerHTML = "00";
   secondsEl.innerHTML = "00";
-  millisecondsEl.innerHTML = "000";
+  millisecondsEl.innerHTML = "00";
   startBtn.style.display = "inline-block";
   pauseBtn.style.display = "none";
   resumeBtn.style.display = "none";
@@ -69,5 +78,5 @@ function formatTime(time) {
 }
 
 function formatMilliseconds(time) {
-  return time < 100 ? `0${time}`.padStart(3, "0") : time;
+  return time < 100 ? `0${time}`.padStart(2, "0") : time;
 }
